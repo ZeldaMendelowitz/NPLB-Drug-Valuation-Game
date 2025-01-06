5365d66be1ba86f94cf05900f11eeb8acf5b2275
Zelda Zara Mendelowitz <zelda.mendelowitz@gmail.com>
2025-01-05 22:05:06 -0500

    Update drug-valuation-game.py

--- drug-valuation-game.py
+++ drug-valuation-game.py
@@ -149,7 +149,7 @@ r_and_d = html.Div(className="sixC",
 
 ############################# START THE APP ####################################
 app = Dash(__name__)
-#server = app.server
+server = app.server
 
 
 
